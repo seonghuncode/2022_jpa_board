6522f745ed379e5d2f26c8820d6f41a75f2e2df1
seonghuncode <gnos456@naver.com>
2022-03-27 15:48:19 +0900

    게시물 1개 조회 기능 구현

--- src/main/java/com/ysh/exam/jpaBoard/article/controller/articleController.java
+++ src/main/java/com/ysh/exam/jpaBoard/article/controller/articleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usr/article")
@@ -22,5 +23,15 @@ public class articleController {
         return articleRepository.findAll();
 
     }
+
+    @RequestMapping("detail")
+    @ResponseBody
+    // EX : http://localhost:8082/usr/article/detail?id=2 => id 매개변수에 2L 이 들어옵니다.
+                            //id는 integer형태로 들어오는데  L운 자바 리터럴로 long으로 버꾸어 준다?
+    public Article showDetail(long id){
+        Optional<Article> article = articleRepository.findById(id);
+        return article.get();
+    }
+    //jpa데이터 한개 -> 구글링 참고
 
 }
