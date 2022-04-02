187b6d646dace9454a4ee27456ee95cc28cf8cc5
seonghuncode <gnos456@naver.com>
2022-04-02 17:45:02 +0900

    ㅇ이이ë메일이 동일한 회ã원이 생기지 않도록 DB수준에서 ë§기

--- db.sql
+++ db.sql
@@ -3,7 +3,6 @@ DROP DATABASE IF EXISTS jpaBoard;
 CREATE DATABASE jpaBoard;
 USE jpaBoard;
 
-
 #게시물(article)
 #-id
 #-등록날짜
@@ -63,3 +62,70 @@ update_date = NOW(),
 title = '제목5',
 `body` = '내용5',
 `user_id` = 5;
+
+SELECT * FROM article;
+
+
+
+
+
+
+# 회원 테이블 생성
+CREATE TABLE `user` (
+    id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    reg_date DATETIME NOT NULL,
+    update_date DATETIME NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    `password` VARCHAR(150) NOT NULL,
+    `name` CHAR(50) NOT NULL
+);
+
+SHOW TABLES;
+
+# 회원데이터 생성
+INSERT INTO `user`
+SET reg_date = NOW(),
+update_date = NOW(),
+email = 'use1@test.com',
+`password` = '1234',
+`name` = '유저1';
+
+INSERT INTO `user`
+SET reg_date = NOW(),
+update_date = NOW(),
+email = 'use2@test.com',
+`password` = '1234',
+`name` = '유저2';
+
+INSERT INTO `user`
+SET reg_date = NOW(),
+update_date = NOW(),
+email = 'use3@test.com',
+`password` = '1234',
+`name` = '유저3';
+
+INSERT INTO `user`
+SET reg_date = NOW(),
+update_date = NOW(),
+email = 'use3@test.com',
+`password` = '1234',
+`name` = '유저4';
+
+
+INSERT INTO `user`
+SET reg_date = NOW(),
+update_date = NOW(),
+email = 'use3@test.com',
+`password` = '1234',
+`name` = '유저5';
+
+SELECT * FROM `user`;
+
+
+
+#user테이블 -> F6 -> unique를 걸어주면 중복이 안된다.
+#or 명령어 사용 // 하지만 이렇게 할 경우 중복 발생시 웹페이지에 에러페이지를 보여 주게 된다.
+INSERT INTO `user` ADD UNIQUE INDEX(email);
+
+
+
--- src/main/java/com/ysh/exam/jpaBoard/user/controller/UserController.java
+++ src/main/java/com/ysh/exam/jpaBoard/user/controller/UserController.java
@@ -20,6 +20,8 @@ public class UserController {
     @ResponseBody
     public String showJoin(String name, String email, String password){
 
+
+
         if(name == null  || name.trim().length() == 0){
             return "name을 입력해주세요";
         }
@@ -36,20 +38,22 @@ public class UserController {
             return "password를 입력해 주세요";
         }
 
-        password = password.trim();
 
-        User user = new User();
+        else {
+            password = password.trim();
 
-        user.setRegDate(LocalDateTime.now());
-        user.setUpdateDate(LocalDateTime.now());
-        user.setName(name);
-        user.setEmail(email);
-        user.setPassword(password);
+            User user = new User();
 
-        userRepository.save(user);
+            user.setRegDate(LocalDateTime.now());
+            user.setUpdateDate(LocalDateTime.now());
+            user.setName(name);
+            user.setEmail(email);
+            user.setPassword(password);
 
-        return "%d번 회원이 생성 되었습니다.".formatted(user.getId());
+            userRepository.save(user);
 
+            return "%d번 회원이 생성 되었습니다.".formatted(user.getId());
+        }
 
 
     }
