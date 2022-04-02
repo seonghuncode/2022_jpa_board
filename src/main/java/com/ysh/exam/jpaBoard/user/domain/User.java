52218dae82c64eebbc044a9a4df0963006902bdd
seonghuncode <gnos456@naver.com>
2022-04-02 16:27:26 +0900

    비밀번호는 json대상에서 제외

--- src/main/java/com/ysh/exam/jpaBoard/user/domain/User.java
+++ src/main/java/com/ysh/exam/jpaBoard/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.ysh.exam.jpaBoard.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +23,8 @@ public class User {
     private LocalDateTime updateDate;
     private String name;
     private String email;
+
+    @JsonIgnore
     private String password;
 
 
